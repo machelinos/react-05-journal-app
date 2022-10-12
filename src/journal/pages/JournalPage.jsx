c8c9cfe99a2ca13bf495dd5403b547861d11bedc
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-10-12 01:17:11 -0500

    Journal App - Note View

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,14 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedView } from "../views/NothingSelectedView"
+import { NoteView } from "../views"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+
+      <NoteView />
     </JournalLayout>
   )
 }
---
+++ src/journal/views/NoteView.jsx
@@ -0,0 +1,48 @@
+import SaveOutlined from "@mui/icons-material/SaveOutlined"
+import { Button, Grid, TextField, Typography } from "@mui/material"
+
+export const NoteView = () => {
+  return (
+    <Grid container direction="row" justifyContent="space-between" alignItems="center" sx={{mb: 1}} >
+        <Grid item>
+            <Typography fontSize={39} fontWeight="light">October 10th 2022</Typography>
+        </Grid>
+
+        <Grid item>
+            <Button color="primary" sx={{padding: 2}}>
+                <SaveOutlined sx={{fontSize: 30, mr: 1}} />
+                Save
+            </Button>
+        </Grid>
+
+        <Grid container direction="column">
+            <Grid item>
+                <TextField
+                    fullWidth
+                    label="Title"
+                    placeholder="Add the title"
+                    type="text"
+                    variant="filled"
+                    sx={{border: 'none', mb: 1}}
+                />
+            </Grid>
+
+            <Grid item>
+                <TextField
+                    fullWidth
+                    label="Title"
+                    minRows={5}
+                    multiline
+                    placeholder="What happened today?"
+                    type="text"
+                    variant="filled"
+                    sx={{border: 'none', mb: 1}}
+                />
+            </Grid>
+
+        </Grid>
+
+    </Grid>
+
+  )
+}
