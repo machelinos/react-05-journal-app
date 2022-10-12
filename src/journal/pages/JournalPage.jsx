23ac405ff48a623f77a1a10bd508d59f658aa466
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-10-12 02:04:22 -0500

    Journal App - Fab button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,29 @@
-import { Typography } from "@mui/material"
+import AddOutlined from "@mui/icons-material/AddOutlined"
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NoteView } from "../views"
-import { NothingSelectedView } from "../views"
+import { NoteView, NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedView /> */}
+      <NothingSelectedView />
 
-      <NoteView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
+          position:'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{fontSize: 30}} />
+      </IconButton>
+
     </JournalLayout>
   )
 }
